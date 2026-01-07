082c0bc61d4dca4d56e7a0cd161d3726b57ed25e
IshantheG <gehlautishan@gmail.com>
2026-01-06 19:10:13 -0500

    Readying for Deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from .routes.endpoints import router
+from backend.app.routes.endpoints import router
 
 api = FastAPI()
 
